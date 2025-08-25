c0850745fd42878d9d99a93e7559c572799eebb6
David Zhou <dzhou@cochlear.com>
2025-08-26 08:50:00 +1000

    Added ignorecase in options

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -9,6 +9,7 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.ruler = true
 vim.opt.shiftwidth = 4
+vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.softtabstop = 4
 vim.opt.tabstop = 4
